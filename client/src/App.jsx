65f54995833f4b65ef0bb1ebdde9b9f3da97c2d4
Tidbits JS <tidbitsjs@gmail.com>
2022-03-13 22:50:52 +0530

    project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { Navbar } from "./components";
+
+const App = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}", "./src/components/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
